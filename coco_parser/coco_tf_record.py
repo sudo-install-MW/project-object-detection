50d71029d1fae5bfed5ba11c0f444862d6cde6ed
Maheshwaran Umapathy XPS <maheshwaran.umapathy@my.utsa.edu>
2018-12-04 18:36:45 -0600

    working towards tf_record creation

--- coco_parser/coco_tf_record.py
+++ coco_parser/coco_tf_record.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+from coco_parser.utils.util_script import CocoTFRecordConverter as CocoRecordConverter
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--anno_path", help="provide path for the coco .json anno file")
@@ -8,47 +10,7 @@ args = parser.parse_args()
 
 # path for annotations in the coco dataset
 anno_path = '/media/maheshwaran/My Passport/Andromeda/dataset/object_det/ms_coco/ms_coco_2017/annotations_trainval2017/annotations/instances_val2017.json'
+cat_dict = {1: 'person', 3: 'car'}
 
-# open the .json file
-with open(anno_path) as json_file:
-    json_file = json_file.read()
-    data = json.loads(json_file)
-
-anno_dict = dict()
-img_spec = dict()
-cat_dict = {1:'person', 3:'car'}
-
-# loop creates a dictionary with image id as key and file_name and h, w as inner dict
-for image_spec in data['images']:
-    buff_dict = dict()
-    for key, value in image_spec.items():
-        if key == 'file_name':
-            buff_dict['file_name'] = value
-        if key == 'height':
-            buff_dict['height'] = value
-        if key == 'width':
-            buff_dict['width'] = value
-
-    img_spec[image_spec['id']] = buff_dict
-
-for anno in data['annotations']:
-    # if annotations category id matches with what we want
-    # add it to the annotation dictionary
-    if anno['category_id'] in cat_dict:
-        anno_buff_dict = dict()
-        for key, value in anno.items():
-            if key == 'category_id':
-                anno_buff_dict['category_id'] = value
-            if key == 'bbox':
-                anno_buff_dict['bbox'] = value
-
-        # if image has no entry of annotation
-        if anno['image_id'] not in anno_dict:
-            anno_dict[anno['image_id']] = [anno_buff_dict]
-
-        # if image has an annotation already
-        else:
-            anno_dict[anno['image_id']].append(anno_buff_dict)
-
-
-print(anno_dict)
+tf_record_creater = CocoRecordConverter(cat_dict, anno_path)
+img_spec_dict, anno_dict = tf_record_creater.dict_initializer()
--- coco_parser/utils/util_script.py
+++ coco_parser/utils/util_script.py
@@ -1,38 +1,79 @@
-anno_dict = dict()
-img_spec = dict()
-cat_dict = {1:'person', 3:'car'}
+import json
 
 
-# loop creates a dictionary with image id as key and file_name and h, w as inner dict
-for image_spec in data['images']:
-    buff_dict = dict()
-    for key, value in image_spec.items():
-        if key == 'file_name':
-            buff_dict['file_name'] = value
-        if key == 'height':
-            buff_dict['height'] = value
-        if key == 'width':
-            buff_dict['width'] = value
+class CocoTFRecordConverter:
 
-    img_spec[image_spec['id']] = buff_dict
+    def __init__(self, cat_dict, anno_path):
 
-for anno in data['annotations']:
-    # if annotations category id matches with what we want
-    # add it to the annotation dictionary
-    if anno['category_id'] in cat_dict:
-        anno_buff_dict = dict()
-        for key, value in anno.items():
-            if key == 'category_id':
-                anno_buff_dict['category_id'] = value
-            if key == 'bbox':
-                anno_buff_dict['bbox'] = value
+        # TODO can you create tf_rec_img_dict with one dictionary?
+        self.anno_dict = dict()
+        self.img_spec = dict()
+        self.tf_rec_img_dict = dict()
+        self.cat_dict = cat_dict
 
-        # if image has no entry of annotation
-        if anno['image_id'] not in anno_dict:
-            anno_dict[anno['image_id']] = [anno_buff_dict]
+        # open the .json file
+        with open(anno_path) as json_file:
+            json_file = json_file.read()
+            self.data = json.loads(json_file)
 
-        # if image has an annotation already
-        else:
-            anno_dict[anno['image_id']].append(anno_buff_dict)
+    def dict_initializer(self):
 
+        # loop creates a dictionary with image id as key and file_name and h, w as inner dict
+        for image_spec in self.data['images']:
+            buff_dict = dict()
+            for key, value in image_spec.items():
+                if key == 'file_name':
+                    buff_dict['file_name'] = value
+                if key == 'height':
+                    buff_dict['height'] = value
+                if key == 'width':
+                    buff_dict['width'] = value
 
+            self.img_spec[image_spec['id']] = buff_dict
+
+        for anno in self.data['annotations']:
+            # if annotations category id matches with what we want
+            # add it to the annotation dictionary
+            if anno['category_id'] in self.cat_dict:
+                anno_buff_dict = dict()
+                for key, value in anno.items():
+                    if key == 'category_id':
+                        anno_buff_dict['category_id'] = value
+                    if key == 'bbox':
+                        anno_buff_dict['bbox'] = value
+
+                # if image has no entry of annotation
+                if anno['image_id'] not in self.anno_dict:
+                    self.anno_dict[anno['image_id']] = [anno_buff_dict]
+
+                # if image has an annotation already
+                else:
+                    self.anno_dict[anno['image_id']].append(anno_buff_dict)
+
+        return self.img_spec, self.anno_dict
+
+    def absolute_bbox_converter(self):
+
+        for images, annotations in self.anno_dict.items():
+            abs_anno_list = []
+            abs_anno_buff_dict = dict()
+
+            # get image specs from self.img_spec
+            file_name = self.img_spec[images]['file_name']
+            img_height = self.img_spec[images]['height']
+            img_width = self.img_spec[images]['width']
+
+            # create the dictionary with absolute annotations
+            for sub_annotations in annotations:
+                x_max, y_max, x_min, y_min = sub_annotations['bbox']
+                cat_id = sub_annotations['category_id']
+                abs_bbox = [x_max/img_height, x_max/img_height, y_max/img_width, x_min/img_width]
+
+                abs_anno_buff_dict['abs_bbox'] = abs_bbox
+                abs_anno_buff_dict['category_id'] = cat_id
+
+                abs_anno_list.append(abs_anno_buff_dict)
+
+            self.tf_rec_img_dict[file_name] = abs_anno_list
+
+        return self.tf_rec_img_dict
